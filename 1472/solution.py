8bbcf611cafe4b53f4adddf20b3a0ef75afb4e5e
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-10 17:22:59 +0800

    1472

---
+++ 1472/solution.py
@@ -0,0 +1,40 @@
+import unittest
+from typing import List, Optional
+
+
+class BrowserHistory:
+    def __init__(self, homepage: str):
+        self.history: List = [homepage]
+        self.curr: int = 0
+
+    def visit(self, url: str) -> None:
+        self.curr += 1
+        self.history = self.history[: self.curr]
+        self.history.append(url)
+
+    def back(self, steps: int) -> str:
+        self.curr = max(self.curr - steps, 0)
+        return self.history[self.curr]
+
+    def forward(self, steps: int) -> Optional[str]:
+        self.curr = min(self.curr + steps, len(self.history) - 1)
+        return self.history[self.curr]
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        obj = BrowserHistory("leetcode.com")
+        obj.visit("google.com")
+        obj.visit("facebook.com")
+        obj.visit("youtube.com")
+        self.assertEqual(obj.back(1), "facebook.com")
+        self.assertEqual(obj.back(1), "google.com")
+        self.assertEqual(obj.forward(1), "facebook.com")
+        obj.visit("linkedin.com")
+        self.assertEqual(obj.forward(2), "linkedin.com")
+        self.assertEqual(obj.back(2), "google.com")
+        self.assertEqual(obj.back(7), "leetcode.com")
+
+
+if __name__ == "__main__":
+    unittest.main()
