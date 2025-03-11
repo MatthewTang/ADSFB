03f7765b44cb6c85246b60a884d61660b38e01e8
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-11 22:40:39 +0800

    1700 setup

---
+++ 1700/solution.py
@@ -0,0 +1,18 @@
+import unittest
+from typing import List, Optional
+
+# 2240
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        return 0
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        s = Solution()
+
+
+if __name__ == "__main__":
+    unittest.main()
