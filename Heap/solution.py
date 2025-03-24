4f4d3746e33c49bb42fb96638c653721e3a7c385
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-24 21:50:16 +0800

    heap

---
+++ Heap/solution.py
@@ -0,0 +1,15 @@
+import unittest
+from typing import Optional, List
+from collections import deque
+
+class Heap:
+    def __init__(self) -> None:
+        self.heap = [0]
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        return
+
+if __name__ == "__main__":
+    unittest.main()
