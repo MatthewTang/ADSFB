44aafac4d487c5ba34698029012e7bd7946b2daf
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-26 02:10:36 +0800

    703 update

--- 703/solution.py
+++ 703/solution.py
@@ -1,5 +1,6 @@
 import unittest
 from typing import List, Optional
+import heapq
 
 
 # # naive
@@ -8,29 +9,43 @@ from typing import List, Optional
 #     def __init__(self, k: int, nums: List[int]) -> None:
 #         self.arr = sorted(nums)
 #         self.k = k
-#         pass
 #
-#     # O(nlog(n))
+#     # O(m * nlog(n)), O(m) is the no. of times add() called, O(n) is the size of the array
 #     def add(self, val: int) -> int:
 #         self.arr.append(val)
 #         self.arr = sorted(self.arr)
 #         return self.arr[-self.k]
 
+# maintain array of size k
+# class KthLargest:
+#     # O(nlog(n))
+#     def __init__(self, k: int, nums: List[int]) -> None:
+#         self.arr = sorted(nums + [-float("infinity")])[-k:]
+#
+#     # O(m*klog(k)), m is no. of times add() called, k is the size of the array
+#     def add(self, val: int) -> int:
+#         if self.arr[0] >= val:
+#             return self.arr[0]
+#         else:
+#             self.arr[0] = val
+#             self.arr = sorted(self.arr)
+#             return self.arr[0]
+
 # optimal, maintain min heap of size k
 class KthLargest:
-    # O(nlog(n))
+    # time: O(nlog(k)), space: O(k)
     def __init__(self, k: int, nums: List[int]) -> None:
-        self.arr = sorted(nums + [-float("infinity")])[-k:]
-        pass
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
 
-    # O(mlog(k)), m is no. of times add() called, k is the size of the array
+    # time: O(log(k)), space: O(k)
     def add(self, val: int) -> int:
-        if self.arr[0] >= val:
-            return self.arr[0]
-        else:
-            self.arr[0] = val
-            self.arr = sorted(self.arr)
-            return self.arr[0]
+        heapq.heappush(self.minHeap, val)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
 
 
 class Test(unittest.TestCase):
