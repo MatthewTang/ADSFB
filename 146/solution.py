7aadc27e08ff93705636cf2573158cfd76dd8b38
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-29 01:26:59 +0800

    146 update

--- 146/solution.py
+++ 146/solution.py
@@ -1,6 +1,6 @@
-from collections import deque
+from collections import OrderedDict
 import unittest
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, OrderedDict
 
 
 # class LRUCache:
@@ -39,72 +39,95 @@ from typing import Dict, List, Optional
 #         self.map[key] = [value, self.age]
 
 
-class Node:
-    def __init__(
-        self, val: int, prev: Optional["Node"] = None, next: Optional["Node"] = None
-    ) -> None:
-        self.val: int = val  # key
-        self.prev: Optional[Node] = prev
-        self.next: Optional[Node] = next
+# class Node:
+#     def __init__(
+#         self, val: int, prev: Optional["Node"] = None, next: Optional["Node"] = None
+#     ) -> None:
+#         self.val: int = val  # key
+#         self.prev: Optional[Node] = prev
+#         self.next: Optional[Node] = next
+#
+#
+# # space: O(n)
+# class LRUCache:
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.map1: Dict[int:int] = {}  # key -> value
+#         self.map2: Dict[int:Node] = {}  # key -> Node
+#         self.head = Node(0)  # easier for add/remove first
+#         self.tail = Node(0)
+#         self.head.next = self.tail
+#         self.tail.prev = self.head
+#
+#     # O(1)
+#     def remove(self, node: Node):
+#         node.prev.next = node.next
+#         node.next.prev = node.prev
+#
+#     # O(1)
+#     def insert(self, node: Node):
+#         node.prev = self.tail.prev
+#         node.next = self.tail
+#         self.tail.prev.next = node
+#         self.tail.prev = node
+#
+#     # O(1)
+#     def get(self, key: int) -> int:
+#         if key not in self.map1:
+#             return -1
+#         node: Node = self.map2[key]
+#         self.remove(node)
+#         self.insert(node)
+#         return self.map1[key]
+#
+#     # O(1)
+#     def put(self, key: int, value: int) -> None:
+#
+#         if key in self.map1:
+#             node: Node = self.map2[key]
+#             self.remove(node)
+#             self.insert(node)
+#             self.map1[key] = value
+#             return
+#
+#         if len(self.map1) < self.capacity:
+#             self.map1[key] = value
+#             node = Node(key)
+#             self.insert(node)
+#             self.map2[key] = node
+#             return
+#
+#         self.map1[key] = value
+#         node = Node(key)
+#         self.insert(node)
+#         self.map2[key] = node
+#
+#         head: Node = self.head.next
+#         del self.map1[head.val]
+#         self.remove(head)
 
 
-# space: O(n)
 class LRUCache:
-    def __init__(self, capacity: int):
+    def __init__(self, capacity: int) -> None:
+        self.cache: OrderedDict = OrderedDict()  # using doubly-linked list w hash table
         self.capacity = capacity
-        self.map1: Dict[int:int] = {}  # key -> value
-        self.map2: Dict[int:Node] = {}  # key -> Node
-        self.head = Node(0)  # easier for add/remove first
-        self.tail = Node(0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        pass
 
-    # O(1)
-    def remove(self, node: Node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    # O(1)
-    def insert(self, node: Node):
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-
-    # O(1)
     def get(self, key: int) -> int:
-        if key not in self.map1:
+        if key not in self.cache:  # O(1)
             return -1
-        node: Node = self.map2[key]
-        self.remove(node)
-        self.insert(node)
-        return self.map1[key]
+        self.cache.move_to_end(key)  # O(1)
+        return self.cache[key]  # O(1)
 
-    # O(1)
-    def put(self, key: int, value: int) -> None:
+    def put(self, key: int, val: int) -> None:
+        if key in self.cache:  # O(1)
+            self.cache.move_to_end(key)  # O(1)
+        self.cache[key] = val  # O(1)
 
-        if key in self.map1:
-            node: Node = self.map2[key]
-            self.remove(node)
-            self.insert(node)
-            self.map1[key] = value
-            return
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(False)  # O(1)
 
-        if len(self.map1) < self.capacity:
-            self.map1[key] = value
-            node = Node(key)
-            self.insert(node)
-            self.map2[key] = node
-            return
-
-        self.map1[key] = value
-        node = Node(key)
-        self.insert(node)
-        self.map2[key] = node
-
-        head: Node = self.head.next
-        del self.map1[head.val]
-        self.remove(head)
+        return
 
 
 class Test(unittest.TestCase):
