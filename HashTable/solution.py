beb1dd16c7a513504f5e939929b45fed2b758262
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-29 02:19:54 +0800

    hash table setup

---
+++ HashTable/solution.py
@@ -0,0 +1,31 @@
+import unittest
+from typing import Optional, List
+from collections import deque
+
+
+class HashTable:
+    def __init__(self, capacity: int):
+        return
+
+    def insert(self, key: int, value: int) -> None:
+        return
+
+    def get(self, key: int) -> int:
+        return
+
+    def remove(self, key: int) -> bool:
+        return
+
+    def getSize(self) -> int:
+        return
+
+    def getCapacity(self) -> int:
+        return
+
+    def resize(self) -> None:
+        return
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        return
