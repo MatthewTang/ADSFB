4a53d6f6a4aa5bc09d4029ce24eb4927812e9ef3
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-29 14:23:29 +0800

    hash table open addressing

--- HashTable/solution.py
+++ HashTable/solution.py
@@ -3,29 +3,99 @@ from typing import Optional, List
 from collections import deque
 
 
+class Pair:
+    def __init__(self, key: int, val: int) -> None:
+        self.key = key
+        self.val = val
+
+
+# open addressing
 class HashTable:
     def __init__(self, capacity: int):
-        return
+        self.size = 0
+        self.capacity = capacity
+        self.map: List[Optional[Pair]] = [None] * capacity
 
     def insert(self, key: int, value: int) -> None:
-        return
+        index = self.hash(key)
+        while self.map[index] != None:
+            if self.map[index].key == key:
+                self.map[index].val = value
+                return
+            index += 1
+            index % self.capacity
+
+        self.map[index] = Pair(key, value)
+        self.size += 1
+        if self.size >= self.capacity // 2:
+            self.resize()
 
     def get(self, key: int) -> int:
-        return
+        index = self.hash(key)
+        while self.map[index] != None:
+            if self.map[index].key == key:
+                return self.map[index].val
+
+        return -1
 
     def remove(self, key: int) -> bool:
-        return
+        index = self.hash(key)
+        while self.map[index] != None:
+            if self.map[index].key == key:
+                self.map[index] = None
+                return True
+
+        return False
 
     def getSize(self) -> int:
-        return
+        return self.size
 
     def getCapacity(self) -> int:
-        return
+        return self.capacity
 
     def resize(self) -> None:
-        return
+        self.capacity = self.capacity * 2
+        oldMap = self.map
+        self.map = [None] * self.capacity
+        self.size = 0
+        for i in oldMap:
+            if i:
+                self.insert(i.key, i.val)
+
+    def hash(self, key: int) -> int:
+        return key % self.capacity
 
 
 class Test(unittest.TestCase):
     def test1(self):
-        return
+        hashTable = HashTable(4)
+        self.assertIsNone(hashTable.insert(1, 2))
+        self.assertIs(hashTable.get(1), 2)
+        self.assertIsNone(hashTable.insert(1, 3))
+        self.assertIs(hashTable.get(1), 3)
+        self.assertTrue(hashTable.remove(1))
+        self.assertIs(hashTable.get(1), -1)
+
+    def test2(self):
+        hashTable = HashTable(2)
+        self.assertIs(hashTable.getCapacity(), 2)
+        self.assertIsNone(hashTable.insert(6, 7))
+        self.assertIs(hashTable.getCapacity(), 4)
+        self.assertIsNone(hashTable.insert(1, 2))
+        self.assertIs(hashTable.getCapacity(), 8)
+        self.assertIsNone(hashTable.insert(3, 4))
+        self.assertIs(hashTable.getCapacity(), 8)
+        self.assertIs(hashTable.getSize(), 3)
+
+    def test3(self):
+        hashTable = HashTable(4)
+        self.assertIsNone(hashTable.insert(1, 2))
+        self.assertIs(hashTable.get(1), 2)
+        self.assertIsNone(hashTable.insert(1, 3))
+        self.assertIs(hashTable.get(1), 3)
+        self.assertIs(hashTable.remove(1), True)
+        self.assertIs(hashTable.get(1), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
