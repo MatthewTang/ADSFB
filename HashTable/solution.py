73fd5cb8e9de684bf397c8f0810139e3555d3019
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-30 00:03:38 +0800

    hash table chaining

--- HashTable/solution.py
+++ HashTable/solution.py
@@ -10,40 +10,122 @@ class Pair:
 
 
 # open addressing
+# class HashTable:
+#     def __init__(self, capacity: int):
+#         self.size = 0
+#         self.capacity = capacity
+#         self.map: List[Optional[Pair]] = [None] * capacity
+#
+#     def insert(self, key: int, value: int) -> None:
+#         index = self.hash(key)
+#         while self.map[index] != None:
+#             if self.map[index].key == key:
+#                 self.map[index].val = value
+#                 return
+#             index += 1
+#             index % self.capacity
+#
+#         self.map[index] = Pair(key, value)
+#         self.size += 1
+#         if self.size >= self.capacity // 2:
+#             self.resize()
+#
+#     def get(self, key: int) -> int:
+#         index = self.hash(key)
+#         while self.map[index] != None:
+#             if self.map[index].key == key:
+#                 return self.map[index].val
+#
+#         return -1
+#
+#     def remove(self, key: int) -> bool:
+#         index = self.hash(key)
+#         while self.map[index] != None:
+#             if self.map[index].key == key:
+#                 self.map[index] = None
+#                 return True
+#
+#         return False
+#
+#     def getSize(self) -> int:
+#         return self.size
+#
+#     def getCapacity(self) -> int:
+#         return self.capacity
+#
+#     def resize(self) -> None:
+#         self.capacity = self.capacity * 2
+#         oldMap = self.map
+#         self.map = [None] * self.capacity
+#         self.size = 0
+#         for i in oldMap:
+#             if i:
+#                 self.insert(i.key, i.val)
+#
+#     def hash(self, key: int) -> int:
+#         return key % self.capacity
+
+
+class Node:
+    def __init__(self, key: int, val: int, next: Optional["Node"] = None) -> None:
+        self.key = key
+        self.val = val
+        self.next = next
+
+
+# chaining
 class HashTable:
     def __init__(self, capacity: int):
         self.size = 0
         self.capacity = capacity
-        self.map: List[Optional[Pair]] = [None] * capacity
+        self.map: List[Optional[Node]] = [None] * capacity
 
     def insert(self, key: int, value: int) -> None:
         index = self.hash(key)
-        while self.map[index] != None:
-            if self.map[index].key == key:
-                self.map[index].val = value
-                return
-            index += 1
-            index % self.capacity
 
-        self.map[index] = Pair(key, value)
+        node = Node(key, value)
+        curr = self.map[index]
+        if not curr:
+            self.map[index] = node
+        else:
+            while curr:
+                if curr.key == key:
+                    curr.val = value
+                    break
+                if curr.next:
+                    curr = curr.next
+                else:
+                    curr.next = node
+                    break
+
         self.size += 1
         if self.size >= self.capacity // 2:
             self.resize()
 
     def get(self, key: int) -> int:
         index = self.hash(key)
-        while self.map[index] != None:
-            if self.map[index].key == key:
-                return self.map[index].val
+        curr = self.map[index]
+        while curr:
+            if curr.key == key:
+                return curr.val
+            else:
+                curr = curr.next
 
         return -1
 
     def remove(self, key: int) -> bool:
         index = self.hash(key)
-        while self.map[index] != None:
-            if self.map[index].key == key:
-                self.map[index] = None
+        prev, curr = None, self.map[index]
+        while curr:
+            if curr.key == key:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    self.map[index] = curr.next
+                self.size -= 1
                 return True
+            else:
+                prev, curr = curr, curr.next
 
         return False
 
@@ -95,6 +177,21 @@ class Test(unittest.TestCase):
         self.assertIs(hashTable.get(1), 3)
         self.assertIs(hashTable.remove(1), True)
         self.assertIs(hashTable.get(1), -1)
+
+    def test4(self):
+        hashTable = HashTable(8)
+        self.assertIsNone(hashTable.insert(8, 0))
+        self.assertIsNone(hashTable.insert(16, 0))
+        self.assertIsNone(hashTable.insert(24, 0))
+        self.assertIs(hashTable.get(8), 0)
+        self.assertIs(hashTable.get(16), 0)
+        self.assertIs(hashTable.get(24), 0)
+        self.assertIs(hashTable.remove(24), True)
+        self.assertIs(hashTable.get(24), -1)
+        self.assertIs(hashTable.remove(16), True)
+        self.assertIs(hashTable.get(16), -1)
+        self.assertIs(hashTable.remove(8), True)
+        self.assertIs(hashTable.get(8), -1)
 
 
 if __name__ == "__main__":
