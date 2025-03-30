cfa61ca76808ff633acb345b6515b1f3b585b7fc
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-03-31 00:03:30 +0800

    200

--- 200/solution.py
+++ 200/solution.py
@@ -1,10 +1,44 @@
 import unittest
-from typing import List, Optional
+from typing import List, Optional, Set
 
 
 class Solution:
-    def numIslands(self, grid: List[List[int]]) -> int:
-        return 0
+    # time: O(m*n), space: O(m*n)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row, col = len(grid), len(grid[0])
+
+        def dfs(r, c, visited: Set) -> None:
+            # out of bounds
+            if r < 0 or c < 0 or r >= row or c >= col:
+                return
+            # sea
+            if grid[r][c] == "0":
+                return
+            # visited
+            if (r, c) in visited:
+                return
+
+            visited.add((r, c))
+
+            dfs(r - 1, c, visited)
+            dfs(r, c + 1, visited)
+            dfs(r + 1, c, visited)
+            dfs(r, c - 1, visited)
+
+        visited = set()
+        islands = 0
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == "0":
+                    continue
+                if (r, c) in visited:
+                    continue
+
+                dfs(r, c, visited)
+                islands += 1
+
+        return islands
 
 
 class Test(unittest.TestCase):
@@ -29,6 +63,16 @@ class Test(unittest.TestCase):
             ["0", "0", "0", "1", "1"],
         ]
         expected = 3
+        result = s.numIslands(grid)
+        self.assertIs(result, expected)
+
+    def test3(self):
+        s = Solution()
+        grid = [
+            ["1", "0"],
+            ["0", "1"],
+        ]
+        expected = 2
         result = s.numIslands(grid)
         self.assertIs(result, expected)
 
