7a1be769604f960961cef106b534ee2ef83bf4fd
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-10 17:24:29 +0800

    adjacency list setup

---
+++ Graph/solution.py
@@ -0,0 +1,25 @@
+import unittest
+from typing import List, Optional
+
+
+class Graph:
+    def __init__(self) -> None:
+        pass
+
+    def addEdge(self, src: int, dst: int) -> None:
+        pass
+
+    def removeEdge(self, src: int, dst: int) -> None:
+        pass
+
+    def removeEdge(self, src: int, dst: int) -> None:
+        pass
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
