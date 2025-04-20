91c5e0ffb14ff71fd54dc4fc7715ae9f80c90ebf
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-20 17:40:19 +0800

    2-d dp update

--- 2-DimensionDP/solution.py
+++ 2-DimensionDP/solution.py
@@ -1,11 +1,12 @@
 from collections import deque
 import unittest
 from typing import Deque, List, Optional, Set, Tuple
+from functools import lru_cache
 
 
-# 33
+# 33. 2-Dimension DP
 class Solution:
-    # # time: O(2^(m*n))
+    # # dfs, time: O(2^(m+n)), m+n avg length of each path, hence height of decision tree
     # def countPaths(self, grid: List[List[int]]) -> int:
     #     ROW, COL = len(grid), len(grid[0])
     #     directions = [(1, 0), (0, 1)]
@@ -33,21 +34,22 @@ class Solution:
     #
     #     return dfs(0, 0, set())
 
-    # optimised, time: O(2^(m*n))
-    def countPaths(self, grid: List[List[int]]) -> int:
-        ROW, COL = len(grid), len(grid[0])
+    # # dfs optimised, time: O(2^(m+n))
+    # def countPaths(self, grid: List[List[int]]) -> int:
+    #     ROW, COL = len(grid), len(grid[0])
+    #
+    #     def dfs(r: int, c: int):
+    #         if r == ROW - 1 and c == COL - 1:
+    #             return 1
+    #
+    #         if r >= ROW or c >= COL:
+    #             return 0
+    #
+    #         return dfs(r + 1, c) + dfs(r, c + 1)
+    #
+    #     return dfs(0, 0)
 
-        def dfs(r: int, c: int):
-            if r == ROW - 1 and c == COL - 1:
-                return 1
-
-            if r >= ROW or c >= COL:
-                return 0
-
-            return dfs(r + 1, c) + dfs(r, c + 1)
-
-        return dfs(0, 0)
-
+    # # bfs, non-standard, O(2^(m+n))
     # def countPaths(self, grid: List[List[int]]) -> int:
     #     ROW, COL = len(grid), len(grid[0])
     #     directions: List[Tuple[int, int]] = [(1, 0), (0, 1)]
@@ -69,43 +71,8 @@ class Solution:
     #             q.append((_r, _c))
     #
     #     return count
-    #
-    # # memoisation, time: O(m*n)
-    # def countPaths(self, grid: List[List[int]]) -> int:
-    #     ROW, COL = len(grid), len(grid[0])
-    #     directions = [(1, 0), (0, 1)]
-    #     cache = [[0] * COL for _ in range(ROW)]
-    #
-    #     def dfs(r: int, c: int, visited: Set[Tuple[int, int]]):
-    #         if r < 0 or c < 0:
-    #             return 0
-    #
-    #         if r >= ROW or c >= COL:
-    #             return 0
-    #
-    #         if (r, c) in visited:
-    #             return 0
-    #
-    #         if cache[r][c]:
-    #             return cache[r][c]
-    #
-    #         if r == ROW - 1 and c == COL - 1:
-    #             cache[r][c] = 1
-    #             return 1
-    #
-    #         visited.add((r, c))
-    #         count = 0
-    #         for dr, dc in directions:
-    #             count += dfs(r + dr, c + dc, visited)
-    #         visited.remove((r, c))
-    #
-    #         cache[r][c] = count
-    #
-    #         return count
-    #
-    #     return dfs(0, 0, set())
 
-    # # optimised, memoised, time: O(2^(m*n))
+    # # dfs optimised and memoised, time: O(m*n), space: O(m*n)
     # def countPaths(self, grid: List[List[int]]) -> int:
     #     ROW, COL = len(grid), len(grid[0])
     #     cache = [[0] * COL for _ in range(ROW)]
@@ -126,7 +93,34 @@ class Solution:
     #
     #     return dfs(0, 0)
 
-    # time: O(m*n)
+    # # 2d dp: time: O(m*n), space: O(m*n)
+    # def countPaths(self, grid: List[List[int]]) -> int:
+    #     m, n = len(grid), len(grid[0])
+    #     dp = [[0] * n for _ in range(m)]
+    #     dp[0][0] = 1
+    #     for r in range(m):
+    #         for c in range(n):
+    #             print(r, c)
+    #             if r > 0:
+    #                 dp[r][c] += dp[r - 1][c]
+    #             print(dp)
+    #             if c > 0:
+    #                 dp[r][c] += dp[r][c - 1]
+    #             print(dp)
+    #
+    #     return dp[m - 1][n - 1]
+
+    # # 1d dp: time: O(m*n), space: O(n)
+    # def countPaths(self, grid: List[List[int]]) -> int:
+    #     ROW, COL = len(grid), len(grid[0])
+    #     dp = [1] * COL
+    #     for _ in range(ROW - 1):
+    #         for i in range(COL):
+    #             dp[i] = dp[i] + dp[i - 1] if i > 0 else 1
+    #
+    #     return dp[-1]
+
+    # 1d dp optimised: time: O(m*n), space: O(n-1) = O(n)
     def countPaths(self, grid: List[List[int]]) -> int:
         ROW, COL = len(grid), len(grid[0])
         dp = [1] * (COL - 1)
@@ -149,6 +143,13 @@ class Test(unittest.TestCase):
         s = Solution()
         grid = [[0] * 3 for _ in range(3)]
         expected = 6
+        result = s.countPaths(grid)
+        self.assertIs(result, expected)
+
+    def test3(self):
+        s = Solution()
+        grid = [[0] * 4 for _ in range(3)]
+        expected = 10
         result = s.countPaths(grid)
         self.assertIs(result, expected)
 
