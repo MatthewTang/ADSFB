ecb72fa0aec7390a718089c2dc383a14b11b095c
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-22 23:55:15 +0800

    1143

--- 1143/solution.py
+++ 1143/solution.py
@@ -4,7 +4,31 @@ from typing import List, Optional
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return
+        def dfs(text: str, i: int, res: List[str]):
+            l = len(text)
+            if i >= l:
+                return
+            c = text[i]
+            _res = [c]
+            for r in res:
+                _res.append(r + c)
+            res += _res
+            dfs(text, i + 1, res)
+
+        res1 = []
+        dfs(text1, 0, res1)
+        # print(res1)
+
+        res2 = []
+        dfs(text2, 0, res2)
+        # print(res2)
+
+        res = 0
+        for r in res1:
+            if r in res2:
+                res = max(res, len(r))
+
+        return res
 
 
 class Test(unittest.TestCase):
@@ -13,6 +37,22 @@ class Test(unittest.TestCase):
         text1 = "abcde"
         text2 = "ace"
         expected = 3
+        result = s.longestCommonSubsequence(text1, text2)
+        self.assertIs(result, expected)
+
+    def test2(self):
+        s = Solution()
+        text1 = "abc"
+        text2 = "abc"
+        expected = 3
+        result = s.longestCommonSubsequence(text1, text2)
+        self.assertIs(result, expected)
+
+    def test3(self):
+        s = Solution()
+        text1 = "abc"
+        text2 = "def"
+        expected = 0
         result = s.longestCommonSubsequence(text1, text2)
         self.assertIs(result, expected)
 
