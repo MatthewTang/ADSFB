415628f8daa1d05909e542f844bddcd21779cb78
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-23 15:05:44 +0800

    1143 update

--- 1143/solution.py
+++ 1143/solution.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 
 
 class Solution:
+    # dfs, time: O(n * 2^n) + O(m * 2^m) + O(2^(n+m)) = O(2^(n+m))
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         def dfs(text: str, i: int, res: List[str]):
             l = len(text)
@@ -10,20 +11,19 @@ class Solution:
                 return
             c = text[i]
             _res = [c]
-            for r in res:
-                _res.append(r + c)
-            res += _res
+            for r in res:  # O(2^i)
+                _res.append(r + c)  # O(i)
+            res += _res  # O(2^i)
             dfs(text, i + 1, res)
 
         res1 = []
-        dfs(text1, 0, res1)
-        # print(res1)
+        dfs(text1, 0, res1)  # O(n * 2^n)
 
         res2 = []
-        dfs(text2, 0, res2)
-        # print(res2)
+        dfs(text2, 0, res2)  # O(m * 2^m)
 
         res = 0
+        # O(2^n * 2^m) = O(2^(n+m))
         for r in res1:
             if r in res2:
                 res = max(res, len(r))
