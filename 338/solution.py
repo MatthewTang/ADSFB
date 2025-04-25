9d8fb944ba4fc2c212f6fac639506c3a7407a63a
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-26 02:00:53 +0800

    338 updated

--- 338/solution.py
+++ 338/solution.py
@@ -3,38 +3,49 @@ from typing import List, Optional
 
 
 class Solution:
-    # time: O(nlogn)
+    # # time: O(nlogn)
+    # def countBits(self, n: int) -> List[int]:
+    #     # time: O(log(n))
+    #     def _countBits(n: int) -> int:
+    #         c = 0
+    #         while n:
+    #             c += 1 if n & 1 else 0
+    #             n >>= 1
+    #         return c
+    #
+    #     res = []
+    #     # O(n)
+    #     for i in range(n + 1):
+    #         res.append(_countBits(i))  # O(log(i))
+    #     return res
+
+    # # time: O(n)
+    # def countBits(self, n: int) -> List[int]:
+    #     # time: O(1)
+    #     def _countBits(n: int) -> int:
+    #         c = 0
+    #         for i in range(17):
+    #             b = 1 << i
+    #             if b & n:
+    #                 c += 1
+    #         return c
+    #
+    #     res = []
+    #     # O(n)
+    #     for i in range(n + 1):
+    #         res.append(_countBits(i))  # O(1)
+    #     return res
+
+    # dp: time: O(n)
     def countBits(self, n: int) -> List[int]:
-        # time: O(log(n))
-        def _countBits(n: int) -> int:
-            c = 0
-            while n:
-                c += 1 if n & 1 else 0
-                n >>= 1
-            return c
+        dp = [0] * (n + 1)
+        offset = 1
 
-        res = []
-        # O(n)
-        for i in range(n + 1):
-            res.append(_countBits(i))  # O(log(i))
-        return res
-
-    # time: O(n)
-    def countBits(self, n: int) -> List[int]:
-        # time: O(1)
-        def _countBits(n: int) -> int:
-            c = 0
-            for i in range(17):
-                b = 1 << i
-                if b & n:
-                    c += 1
-            return c
-
-        res = []
-        # O(n)
-        for i in range(n + 1):
-            res.append(_countBits(i))  # O(1)
-        return res
+        for i in range(1, n + 1):
+            if i == offset * 2:
+                offset *= 2
+            dp[i] = 1 + dp[i - offset]
+        return dp
 
 
 class Test(unittest.TestCase):
