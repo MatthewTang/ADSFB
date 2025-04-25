75fe145ba4210d0207a6a63d178dc4c4edc25081
Tang Siu Hin <matthew.tang@connect.hku.hk>
2025-04-25 17:18:52 +0800

    338

---
+++ 338/solution.py
@@ -0,0 +1,37 @@
+import unittest
+from typing import List, Optional
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def _countBits(n: int) -> int:
+            c = 0
+            while n:
+                c += 1 if n & 1 else 0
+                n >>= 1
+            return c
+
+        res = []
+        for i in range(n + 1):
+            res.append(_countBits(i))
+        return res
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        s = Solution()
+        n = 2
+        expected = [0, 1, 1]
+        result = s.countBits(n)
+        self.assertListEqual(result, expected)
+
+    def test2(self):
+        s = Solution()
+        n = 5
+        expected = [0, 1, 1, 2, 1, 2]
+        result = s.countBits(n)
+        self.assertListEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
